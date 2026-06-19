41fd766330b13a46f2247c1e6c0e59ac7ceb4c8b
Kevin <kevin.polocoser04@gmail.com>
2026-06-19 12:09:51 +0300

    Update models.py

--- src/models.py
+++ src/models.py
@@ -1,1 +1,13 @@
+import numpy as np
 
+def nonideal_rlc_gain(f, R_total, L, C):
+    omega = 2 * np.pi * f
+
+    numerator = R_total
+
+    denominator = np.sqrt(
+        R_total**2 +
+        (omega * L - 1/(omega * C))**2
+    )
+
+    return numerator / denominator
